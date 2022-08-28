419b91f017de7f1d41e430e3584eb8ff26a28b02
totoogg <goncharov.vova2014@gamil.com>
2022-08-28 22:59:22 +0300

    feat: add function helper

---
+++ assets/MythicCards/blue/index.js
@@ -0,0 +1,17 @@
+
+
+const cards = 
+[  "blue1.png",
+  "blue2.png",
+  "blue3.png",
+  "blue4.png",
+  "blue5.png",
+  "blue6.png",
+  "blue7.png",
+  "blue8.png",
+  "blue9.png",
+  "blue10.png",
+  "blue11.png",
+  "blue12.png",]
+
+export default cards
